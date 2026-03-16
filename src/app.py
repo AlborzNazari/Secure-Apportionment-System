ea073894302c64696f906626563236fe582a0593
Alborz Nazari <97196168+AlborzNazari@users.noreply.github.com>
2026-03-16 03:42:16 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from src.apportionment import huntington_hill as allocate_seats
 from src.validators import validate_csv_data, validate_seats_allocation
 from src.logging_setup import logger
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # 10MB max
 
 @app.route('/')
